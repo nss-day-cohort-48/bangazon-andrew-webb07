e7c88070df1a5cf0f11d2e05cafe1b2940a40c33
Andrew Webb <webba@ihopu.org>
2021-08-31 10:00:10 -0500

    created html report of completed orders

--- bangazonreports/views/__init__.py
+++ bangazonreports/views/__init__.py
@@ -1,3 +1,4 @@
 from .connection import Connection
 from .users.favoritesbyuser import user_favorite_list
 from .users.nullorders import null_orders
+from .users.completedorders import completed_orders
