64466dd8e401650f2dc3a448948140d5798450d8
Andrew Webb <webba@ihopu.org>
2021-09-02 13:40:26 -0500

    test payment delete

--- tests/payments.py
+++ tests/payments.py
@@ -2,6 +2,7 @@ import datetime
 import json
 from rest_framework import status
 from rest_framework.test import APITestCase
+from bangazonapi.models import Payment
 
 
 class PaymentTests(APITestCase):
@@ -40,4 +41,22 @@ class PaymentTests(APITestCase):
         self.assertEqual(json_response["expiration_date"], "2024-12-31")
         self.assertEqual(json_response["create_date"], str(datetime.date.today()))
 
-    # TODO: Delete payment type
+    def test_delete_payment_type(self):
+        """
+        Ensure we can delete an existing payment type.
+        """
+        payment_type = Payment()
+        payment_type.merchant_name = "Visa"
+        payment_type.account_number = "12121212121"
+        payment_type.customer_id = 1
+        payment_type.expiration_date = "2020-01-01"
+        payment_type.create_date = "2019-11-11"
+        payment_type.save()
+
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token)
+        response = self.client.delete(f"/paymenttypes/{payment_type.id}")
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+        # GET Payment Type AGAIN TO VERIFY 404 response
+        response = self.client.get(f"/paymenttypes/{payment_type.id}")
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
